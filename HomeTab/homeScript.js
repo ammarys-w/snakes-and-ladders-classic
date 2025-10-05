13c4501d21678c0d9bf15cbd97e9e575ee2f14af
ammarys-w <ammar39876@gmail.com>
2025-10-05 15:00:38 +0300

    Implement popup open and close event handlers

--- HomeTab/homeScript.js
+++ HomeTab/homeScript.js
@@ -5,3 +5,21 @@ playBtn.addEventListener('click', () => {
     const players = playerCountSelect.value
     window.location.href = '../GameTab/index.html'
 })
+
+const instructionsBtn = document.getElementById('instructions-btn')
+const popup = document.getElementById('popup-overlay')
+const closePopup = document.querySelector('.close-popup')
+
+instructionsBtn.addEventListener('click', () => {
+    popup.style.display = 'block'
+})
+
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none'
+})
+
+popup.addEventListener('click', (e) => {
+    if (e.target === popup) {
+        popup.style.display = 'none'
+    }
+})
